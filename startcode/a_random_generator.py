4fc4c16efbda687950140f2ab5bf78a7366fad4c
Navaro Devos <164686546+Navaro1234@users.noreply.github.com>
2025-11-22 16:29:37 +0100

    oefening gemaakt/opgelost

--- startcode/a_random_generator.py
+++ startcode/a_random_generator.py
@@ -1,5 +1,7 @@
 import random
 
+keuze = str(input("Geef je keuze. "))
+
 bnvgg = ["Super", "Mega", "Ultra", "Toffe", "Magische"]
 znvgg = ["Coder", "Ninja", "Expert", "Gamer", "Pokémon"]
 
@@ -8,9 +10,11 @@ def genereer_gebruikersnaam() :
     zngeneratorvgg = random.choice(znvgg)
     return bngeneratorvgg + zngeneratorvgg
 
+def keuze1() :
+    print("10 willekeurige gebruikersnamen:")
+    for _ in range(10):
+        print(genereer_gebruikersnaam())
 
+if keuze == "1" :
+    keuze1()
 
-
-print("10 willekeurige gebruikersnamen:")
-for _ in range(10) :
-    print(genereer_gebruikersnaam())
